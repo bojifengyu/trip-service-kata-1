68cab7e6fb98d09381e0399276e863141074cc5b
aspooner <aspooner@DEV-M-73R3W.paychex.com>
2013-04-12 22:57:29 -0400

    added user.isFriendsWith(), used it to refactor getTripsByUser()

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,30 @@
 package org.craftedsw.tripservicekata.user;
+import static org.craftedsw.tripservicekata.trip.UserBuilder.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class UserTest {
 
+	private static final User BOB = new User();
+	private static final User ANOTHER_USER = new User();
+	private User user;
+
+	@Before
+	public void setUp() {
+		user = aUser().friendsWith(BOB).build();
+
+	}
+	@Test public void
+	should_inform_when_user_is_not_friends_with() {
+		assertThat(user.isFriendsWith(ANOTHER_USER), is(false));
+	}
+	
+	@Test public void
+	should_inform_when_user_is_friends_with() {
+		user.addFriend(ANOTHER_USER);
+		assertThat(user.isFriendsWith(ANOTHER_USER), is(true));
+	}
 }
